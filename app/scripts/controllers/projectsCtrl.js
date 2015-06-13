394bdb984ee07991af1c443593f4dcf9229d0a2f
lelenp <lelenp@ee.pw.edu.pl>
2015-06-13 17:00:38 +0200

    strona dodawania projektu

--- app/scripts/controllers/projectsCtrl.js
+++ app/scripts/controllers/projectsCtrl.js
@@ -1,8 +1,14 @@
 'use strict';
 
 angular.module('agilityApp')
-.controller('projectsCtrl', ['$scope', function($scope) {
+.controller('projectsCtrl', ['$scope','$modal', function($scope, $modal) {
     
-    
+    $scope.addProject = function(){
+        var modalInstance = $modal.open({
+          templateUrl: 'views/windows/addProjectWindow.html',
+          controller: 'addProjectWindowCtrl',
+          size: 'lg'
+        });
+    }
     
 }]);
---
+++ app/scripts/controllers/windows/addProjectWindowCtrl.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('agilityApp')
+.controller('addProjectWindowCtrl', ['$scope', function($scope) {
+    
+    
+}]);
