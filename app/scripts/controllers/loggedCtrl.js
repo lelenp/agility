3f3c508afd201a0f39fa4156ac78a715fa1b0d38
lelenp <lelenp@ee.pw.edu.pl>
2015-06-14 21:15:08 +0200

    podpięcie logiki do wylogowania

--- app/scripts/controllers/loggedCtrl.js
+++ app/scripts/controllers/loggedCtrl.js
@@ -1,6 +1,12 @@
 'use strict';
 
 angular.module('agilityApp')
-.controller('loggedCtrl', ['$scope', function($scope) {
+.controller('loggedCtrl', ['$scope','$state', 'loginService', function($scope, $state, loginService) {
+    
+    $scope.logout = function(){
+        loginService.logout(function(data){
+           $state.go('login'); 
+        });
+    }
     
 }]);
