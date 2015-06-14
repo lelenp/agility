cb7fbeeeb286f22b3512a5eb93ce70e53b581802
lelenp <lelenp@ee.pw.edu.pl>
2015-06-14 21:09:26 +0200

    dodanie spinnera

--- app/scripts/services/utils/remoteService.js
+++ app/scripts/services/utils/remoteService.js
@@ -1,15 +1,17 @@
 'use strict';
 
 angular.module('agilityApp')
-.service('remoteService', ['$http', function($http) {
+.service('remoteService', ['$http','$rootScope', function($http, $rootScope) {
     
     // do usuniecia jak juz bedzie dzialac na discie  !!!!!
     var serverUrl = 'http://reviewpik.cloudapp.net:8080';
     
     this.post = function(url, data, handleResult, handleError) {
+        showLoadingInfo();
         $http.post(serverUrl + url, data).
           success(function(data, status, headers, config) {
                 handleResult(data);
+                hideLoadingInfo();
           }).
           error(function(data, status, headers, config) {
                 if(!angular.isUndefined(handleError)){
@@ -17,13 +19,16 @@ angular.module('agilityApp')
                 } else {
                     console.log('Error in post data to URL: ' + serverUrl + url + ' ' + data);
                 }
+                hideLoadingInfo();
           });
     };
     
     this.get = function(url, handleResult, handleError) {
+        showLoadingInfo();
         $http.get(serverUrl + url).
           success(function(data, status, headers, config) {
                 handleResult(data);
+                hideLoadingInfo();
           }).
           error(function(data, status, headers, config) {
                 if(!angular.isUndefined(handleError)){
@@ -31,7 +36,16 @@ angular.module('agilityApp')
                 } else {
                     console.log('Error in get data from URL: ' + serverUrl + url + ' ' + data);
                 }
+                hideLoadingInfo();
           });
     };
+    
+    function showLoadingInfo(){
+        $rootScope.loadingInfo = true;
+    }
+    
+    function hideLoadingInfo(){
+        $rootScope.loadingInfo = false;
+    }
     
 }]);
